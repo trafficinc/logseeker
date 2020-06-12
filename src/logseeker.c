831916f07deb39f2c55139a2342f0d821a702672
trafficinc <einnor212@gmail.com>
2020-06-12 15:32:31 -0400

    added header

--- src/logseeker.c
+++ src/logseeker.c
@@ -1,45 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-#include <regex.h>
 
+#include "logseeker.h"
 //Title:Vec, Author: https://github.com/rxi <https://github.com/rxi/vec>
 #include "vec.h"
 #include "vec.c"
 
 
-void FindWord(char *word, char *file)
-{
-    char line[1024];
-    FILE *fp = fopen(file, "r");
-    while (fgets(line, sizeof(line), fp) != NULL){
-        if (strstr(line, word) != NULL){
-            printf("%s", line);
-        }
-    }
-}
-
-void FindWordRegEx(char *regeex, char *file)
-{
-    regex_t regexCompiled;
-    char line[1024];
-
-    if (regcomp(&regexCompiled, regeex, REG_EXTENDED)) {
-        printf("Could not compile regular expression.\n");
-    };
-
-    FILE *fp = fopen(file, "r");
-    while (fgets(line, sizeof(line), fp) != NULL){
-        if (regexec(&regexCompiled, line, 0, NULL, 0) == 0){
-            printf("%s", line);
-        }
-    }
-
-}
-
 void display_errors(vec_str_t errors)
 {
     int i;
-    for (i = 0; i < errors.length; i++) {
+    for (i = 0; i < errors.length; i++)
+    {
         printf("%s", errors.data[i]);
     }
 }
